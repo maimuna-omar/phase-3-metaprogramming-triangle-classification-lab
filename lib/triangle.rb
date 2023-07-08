3beb2126d22258fa07039475b7f5a3afbe8e27d6
maimuna omar <maymunaabdi07@gmail.com>
2023-07-08 19:11:16 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    validate_triangle
+  end
+
+  def kind
+    if equilateral?
+      :equilateral
+    elsif isosceles?
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    sides = [@side1, @side2, @side3]
+
+    if sides.any?(&:zero?) || sides.any?(&:negative?)
+      raise TriangleError, "Invalid triangle sides: All sides must be positive."
+    elsif @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
+      raise TriangleError, "Invalid triangle sides: Triangle inequality violated."
+    end
+  end
+
+  def equilateral?
+    @side1 == @side2 && @side2 == @side3
+  end
+
+  def isosceles?
+    @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+  end
+
+  class TriangleError < StandardError
+  end
 end
